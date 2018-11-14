f8b5e016f28dc768af1555b1a11c15ad8dd19ced
Antonio <antoniodaria@msn.com>
2018-11-14 16:16:02 +0000

    User can sign-up feature test

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -8,4 +8,14 @@ RSpec.feature 'Sign up', type: :feature do
     expect(page).to have_selector('#user_email')
     expect(page).to have_selector('#user_password')
   end
+
+  scenario 'allows a user to sign up' do
+    visit('/')
+    click_link('Sign up')
+    fill_in('user_email', with: "testemail@gmail.com")
+    fill_in('user_password', with: "password1234")
+    fill_in('user_password_confirmation', with: "password1234")
+    click_button('Sign up')
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+  end
 end
