a19c392f3f4913899176b04053b69bb90c2e28b1
Florence Hapgood <flohapgood@icloud.com>
2018-11-14 15:01:05 +0000

    Uploading photos feature tested

--- spec/users_can_upload_photos_spec.rb
+++ spec/users_can_upload_photos_spec.rb
@@ -1,10 +1,17 @@
-#remember haven't required rake
 require 'rails_helper.rb'
 
 RSpec.feature 'uploading photos', type: :feature do
 
   scenario 'A user can upload an image' do
-
+    visit ("/items")
+    expect(page).to have_content('Items')
+    click_link ("New Item")
+    expect(page).to have_content('New Item')
+    attach_file('item[image]', File.join(Rails.root + 'spec/fixtures/clueless.jpg'))
+    fill_in "item[colour]", with: "Blue"
+    click_button "Create Item"
+    expect(page).to have_content('Item was successfully created')
+    expect(page).to have_css("img[src*='clueless.jpg']")
   end
 
 end
