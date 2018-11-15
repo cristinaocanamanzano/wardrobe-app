7e9b2d2a78c9d87ccc8020b4af08e2903d6ff9ba
cristinaocanamanzano <cristinaocana1@gmail.com>
2018-11-15 15:26:15 +0000

    Added more tests for sign up feature

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -2,12 +2,14 @@
 
 require 'rails_helper'
 
-RSpec.feature 'Sign up', type: :feature do
-  scenario 'shows sign-up page with email input field' do
+RSpec.feature 'Sign up, log in, log out', type: :feature do
+  scenario 'not logged in user is redirected to log in page' do
     visit '/'
+    expect(page).to have_current_path('/users/sign_in')
     expect(page).to have_selector('#user_email')
     expect(page).to have_selector('#user_password')
   end
+
 
   scenario 'allows a user to sign up' do
     visit('/')
@@ -17,5 +19,14 @@ RSpec.feature 'Sign up', type: :feature do
     fill_in('user_password_confirmation', with: 'password1234')
     click_button('Sign up')
     expect(page).to have_content('Welcome! You have signed up successfully.')
+  end
+
+  scenario 'throws an error if user does not enter email when signing up' do
+    visit('/')
+    click_link('Sign up')
+    fill_in('user_password', with: 'password1234')
+    fill_in('user_password_confirmation', with: 'password1234')
+    click_button('Sign up')
+    expect(page).to have_content("Email can't be blank")
   end
 end
