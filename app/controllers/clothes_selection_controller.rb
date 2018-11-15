207f066da3b0b52edca45c42528982530ec33924
Florence Hapgood <flohapgood@icloud.com>
2018-11-15 16:35:48 +0000

    Feature test for page with 2 pictures

--- app/controllers/clothes_selection_controller.rb
+++ app/controllers/clothes_selection_controller.rb
@@ -1,7 +1,11 @@
 class ClothesSelectionController < ApplicationController
 
   def show
-    @clothes = params[:dress]
+    url = request.original_url
+    array_1 = url.split("?")
+    items_string = array_1[1]
+    @items_array = items_string.split("&")
+
   end
 
 end
