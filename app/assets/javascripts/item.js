7e922a51d829d13eaec0d37f9486c7462c02123c
Laura Jackson <playeronereadystart@gmail.com>
2018-11-18 11:01:28 +0000

    Refactor item.js

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,28 +1,20 @@
 
-// $( document ).ready(function() {
-    //console.log( "ready!" );
-// });
-
+function compareClothes(){
+  var topColour = $('#tops-carousel .slick-current').attr('data-colour');
+  var bottomColour = $('#bottoms-carousel .slick-current').attr('data-colour');
+  $('#matcher-message').html(Colour.matcher(topColour, bottomColour));
+}
 
 
 
 document.addEventListener("turbolinks:load", function(){
 
   $('#tops-carousel').on('afterChange', function(event, slick, currentSlide, nextSlide){
-
-    var topColour = $('#tops-carousel .slick-current').attr('data-colour');
-    var bottomColour = $('#bottoms-carousel .slick-current').attr('data-colour');
-    // console.log($.get('items/' +$('#tops-carousel .slick-current').attr('id')))
-    $('#matcher-message').html(Colour.matcher(topColour, bottomColour));
-
-
+    compareClothes();
   });
 
   $('#bottoms-carousel').on('afterChange', function(event, slick, currentSlide, nextSlide){
-    var topColour = $('#tops-carousel .slick-current').attr('data-colour');
-    var bottomColour = $('#bottoms-carousel .slick-current').attr('data-colour');
-    // console.log($.get('items/' +$('#tops-carousel .slick-current').attr('id')))
-    $('#matcher-message').html(Colour.matcher(topColour, bottomColour));
+    compareClothes();
   });
 
 
