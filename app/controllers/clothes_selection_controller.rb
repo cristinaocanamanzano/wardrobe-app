677b236d3d5902a1ae7ecfe695b4c9487559543e
Florence Hapgood <flohapgood@icloud.com>
2018-11-20 12:18:03 +0000

    Check boxes taken out
    Involved removing unncessary tests and files. User now just uses the clothes carousel to save and compare items

--- app/controllers/clothes_selection_controller.rb
+++ app/controllers/clothes_selection_controller.rb
@@ -1,37 +1,31 @@
 # frozen_string_literal: true
+# 
+# class ClothesSelectionController < ApplicationController
+  # def show
+  #   @item_collection = []
+  #   url = request.original_url
+  #   array_1 = url.split('?')
+  #   items_string = array_1[1]
+  #   @items_array = items_string.split('&')
+  #
+  #   @items_array.each do |item|
+  #     array = item.split('=')
+  #     id_number = array[1]
+  #     @item_collection << garment = Item.find(id_number)
+  #   end
 
-class ClothesSelectionController < ApplicationController
-  def show
-    @item_collection = []
-    url = request.original_url
-    array_1 = url.split('?')
-    items_string = array_1[1]
-    @items_array = items_string.split('&')
-
-    @items_array.each do |item|
-      array = item.split('=')
-      id_number = array[1]
-      @item_collection << garment = Item.find(id_number)
-    end
-
-    main_colour = @item_collection[0].colour
-    secondary_colour = @item_collection[1].colour
-
-    occasion_1 = @item_collection[0].clothes_occasion
-    occasion_2 = @item_collection[1].clothes_occasion
-    puts occasion_1
-    puts occasion_2
-    @item_1_id = @item_collection[0].id
-    @item_2_id = @item_collection[1].id
-    @message_2 = Occasion.new.return_match_message_2(occasion_1, occasion_2)
-
-
-    @message_1 = Colour.new.return_match_message(main_colour, secondary_colour)
-
-   puts @message_1
-   puts @message_2
-
-    @overall_message = Final_Judgement.new.overall_match(@message_1, @message_2)
-  end
-
-end
+    # main_colour = @item_collection[0].colour
+    # secondary_colour = @item_collection[1].colour
+    #
+    # occasion_1 = @item_collection[0].clothes_occasion
+    # occasion_2 = @item_collection[1].clothes_occasion
+    # @item_1_id = @item_collection[0].id
+    # @item_2_id = @item_collection[1].id
+    # @message_2 = Occasion.new.return_match_message_2(occasion_1, occasion_2)
+    #
+    #
+    # @message_1 = Colour.new.return_match_message(main_colour, secondary_colour)
+    # @overall_message = Final_Judgement.new.overall_match(@message_1, @message_2)
+#   end
+#
+# end
