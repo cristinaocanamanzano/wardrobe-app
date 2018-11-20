44e9e549aecdcbdb3315526dd28c59997cac47e5
Florence Hapgood <42243785+FlorenceHapgood@users.noreply.github.com>
2018-11-20 12:51:24 +0000

    Update computer_gives_suggestion_spec.rb

--- spec/features/computer_gives_suggestion_spec.rb
+++ spec/features/computer_gives_suggestion_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "suggestion index page" do
+ describe "Random suggestions", js: true do 
   scenario 'computer gives suggestion' do
     sign_up('testuser1@gmail.com')
-    add_item('clueless.jpg', 'navy', 'top', 'formal')
-    add_item('jumpsuit.jpg', 'navy', 'bottom', 'formal')
-    add_item('jumper.jpeg', 'navy', 'bottom', 'formal')
-    page.check('item-1')
-    page.check('item-2')
-    click_button 'Submit'
+    4.times { add_item("clueless.jpg", 'light-blue', 'top', 'formal')}
+    2.times { add_item("jumper.jpeg", 'yellow', 'bottom', 'formal') }
+    add_item("jumpsuit.jpg", 'yellow', 'bottom', 'formal')
+    click_link 'Clothes Carousel'
+    find('#tops-carousel').find('.slick-next').click
     click_button 'Save combination'
     click_link 'Suggestions'
     expect(page).to have_css("img[src*='clueless.jpg']")
-    expect(page).to have_css("img[src*='jumpsuit.jpg']")
-    expect(page).not_to have_css("img[src*='jumper.jpeg']")
+    expect(page).to have_css("img[src*='jumper.jpeg']")
+    expect(page).not_to have_css("img[src*='jumpsuit.jpg']")
   end
+ end
 end
