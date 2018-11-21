11ca99facf526e3f6dcec23cdcfbdfa326dcc1c6
Antonio <antoniodaria@msn.com>
2018-11-21 12:09:57 +0000

    Add the username feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  attr_accessor :user_name
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  private
+
+  def configure_permitted_parameters
+   attributes = [:user_name, :email, :password]
+   devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+   devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
+
 end
