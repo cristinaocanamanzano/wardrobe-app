351dd9a50468db31f8e0969edbb188696bbd03ec
Cristina María Ocaña Manzano <cristinaocana1@gmail.com>
2019-02-02 16:50:46 +0100

    Test for suggestions controller

--- spec/controllers/suggestions_controller_spec.rb
+++ spec/controllers/suggestions_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SuggestionsController, type: :controller do
+  before(:each) do
+     user = User.create(user_name: "Cris", email: "email@email.com", password: "password")
+  end
 
+  describe "GET /" do
+    it "responds with 302" do
+      get :index
+      expect(response).to have_http_status(302)
+    end
+  end
 end
